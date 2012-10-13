fdcb81211445ee1234c03cfc824f9b11c2c8007f
forange <forange@gmail.com>
2012-10-13 17:06:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Hello World
+===========
+
+
+1) created HelloCloud inside Eclipse
+2) then git init, added new file README.md, commit and push from Tortoise
---
+++ src/github/tutorial/HelloWorld.java
@@ -0,0 +1,12 @@
+package github.tutorial;
+
+//The classic Hello World program!
+class HelloWorld {
+
+ public static void main(String[] args) {
+
+    //Write Hello World to the terminal window
+    System.out.println("Hello World!");
+
+ }
+}
